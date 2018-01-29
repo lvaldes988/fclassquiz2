dda529081b56efcac76aba8279ea11e88a18ae22
liset valdes <lvaldes@techlaunch.io>
2018-01-29 14:59:18 -0500

    first commit

---
+++ test2.js
@@ -0,0 +1,40 @@
+//1. Name and describe the two main operations of a stack (to add and remove data)
+pop and push 
+
+//2.Name and describe the two main operations of a queue (to add and remove data).
+enqueue and dequeue
+
+//3.Draw the tree resulting from adding the following sequence of numbers to an empty tree: Draw the tree resulting from adding the following sequence of numbers to an empty tree: 30, 45, 15, 10, 50, 40, 20, 27
+                30
+    27  20            50  45
+
+    10  15             40
+
+//4.Is this tree balanced? If not, which rotation needs to be done? (Use the following rotation as an example: rightRotation(30), or 
+leftRotation(10))
+
+//5.Now add 29. Is the tree balanced? If not, which rotation needs to be done? (Use the following rotation as an example: rightRotation(30), or leftRotation(10))
+rightRotation(30)
+
+//6.Consider the following tree:
+------5
+---2-----8
+-1--3
+
+Now add 0 to the tree. Which one is the first node to go out of balance?
+
+node 2
+
+//7.How do you fix this node? (Use the following rotation as an example: rightRotation(30), or leftRotation(10))
+
+rightRotation
+
+//8.What are the four main steps of mergesort?
+//1) Find the middle point to divide the array into two halves: 
+//2) Call mergeSort for first half: Call the function mergeSort 
+//3). Call mergeSort for second half: Call the function again mergeSort
+//4) Merge the two halves sorted in step 2 and 3: Call merge
+
+
+
+
